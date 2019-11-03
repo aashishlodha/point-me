677ad84fa16b994bdbbf7bd3267397c6962d88c8
Aashish Lodha <lodhaaashish@gmail.com>
2019-11-04 00:21:54 +0530

    updated web socket path to use wss (secured)

--- point-me-ui/src/environments/environment.prod.ts
+++ point-me-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseApi: 'https://point-me-pocker.herokuapp.com/',
-  baseWs: 'ws://point-me-pocker.herokuapp.com/'
+  baseWs: 'wss://point-me-pocker.herokuapp.com/'
 };
